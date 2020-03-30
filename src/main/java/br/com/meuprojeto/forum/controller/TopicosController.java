df32c269615cf1167dc7ed03a7e68a5cdbadf64c
Igor Frey <igorfreyy97@gmail.com>
2020-03-30 17:27:55 -0300

    Criando validações com as anotações Bean Validation

--- src/main/java/br/com/meuprojeto/forum/controller/TopicosController.java
+++ src/main/java/br/com/meuprojeto/forum/controller/TopicosController.java
@@ -3,6 +3,8 @@ package br.com.meuprojeto.forum.controller;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,14 +43,11 @@ public class TopicosController {
 	}
 
 	@PostMapping
-	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
 		
 		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
 		return ResponseEntity.created(uri).body(new TopicoDto(topico));
 	}
-	
-	
-	
 }
--- src/main/java/br/com/meuprojeto/forum/controller/form/TopicoForm.java
+++ src/main/java/br/com/meuprojeto/forum/controller/form/TopicoForm.java
@@ -1,13 +1,23 @@
 package br.com.meuprojeto.forum.controller.form;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
 import br.com.meuprojeto.forum.model.Curso;
 import br.com.meuprojeto.forum.model.Topico;
 import br.com.meuprojeto.forum.repository.CursoRepository;
 
 public class TopicoForm {
 
+	@NotNull @NotEmpty @Length(min = 5)
 	private String titulo;
+	
+	@NotNull @NotEmpty @Length(min = 10)
 	private String mensagem;
+	
+	@NotNull @NotEmpty
 	private String nomeCurso;
 
 	public String getTitulo() {
