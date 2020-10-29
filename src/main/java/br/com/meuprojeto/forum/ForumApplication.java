b6d87760b8dddb66308970ccf9514a7df37618c0
Igor Frey <igorfreyy97@gmail.com>
2020-10-28 23:57:40 -0300

    Paginação e ordenação

--- src/main/java/br/com/meuprojeto/forum/ForumApplication.java
+++ src/main/java/br/com/meuprojeto/forum/ForumApplication.java
@@ -2,8 +2,11 @@ package br.com.meuprojeto.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class ForumApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/meuprojeto/forum/controller/TopicosController.java
+++ src/main/java/br/com/meuprojeto/forum/controller/TopicosController.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -42,9 +44,7 @@ public class TopicosController {
 
 	@GetMapping
 	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
-			@RequestParam(required = true) int pagina, @RequestParam(required = true) int qtd) {
-		
-		Pageable paginacao = PageRequest.of(pagina, qtd);
+			@PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10) Pageable paginacao) {
 		
 		if (nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
